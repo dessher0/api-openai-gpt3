bbb755da692d5f1e886b1c356daac798097b2b83
dessher0 <github@mcjkula.com>
2022-09-21 01:59:52 +0200

    Added python logic for API connection to pages

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, request, session
-from twilio.twiml.messaging_response import MessagingResponse
-from openai_bot import ask, append_interaction_to_chat_log
+from flask import Flask, render_template, request
+import gpt_3
+import config
+
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '59jk84aojq4'
+app.config.from_object(config.config['development'])
 
-@app.route('/openai', methods=['POST'])
-def openai():
-    incoming_msg = request.values['Body']
-    chat_log = session.get('chat_log')
-    answer = ask(incoming_msg, chat_log)
-    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, chat_log)
+app.register_error_handler(404, page_not_found)
 
-    msg = MessagingResponse()
-    msg.message(answer)
 
-    return str(msg)
+@app.route('/', methods=["GET", "POST"])
+def index():
+
+    if request.method == 'POST':
+        if 'form_1' in request.form:
+            input_text = request.form['TextResult']
+            response = gpt_3.main(input_text)
+            Result = response.replace('\n', '<br>')
+
+    return render_template('index.html', **locals())
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='8000', debug=True)
