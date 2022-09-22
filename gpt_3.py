34a918370e813d987dce4d4d54bedf03ba407265
Maciej <m.kula@netperformers.de>
2022-09-22 20:41:10 +0200

    Restored default API

--- gpt_3.py
+++ gpt_3.py
@@ -1,14 +1,13 @@
-import os
 import openai
 import config
 
 
 openai.api_key = config.OPENAI_API_KEY
 
-def main(input_text):
+def main():
     response = openai.Completion.create(
     model="text-davinci-002",
-    prompt=input_text,
+    prompt=input("Enter your prompt: "),
     temperature=0.3,
     max_tokens=256,
     top_p=1,
